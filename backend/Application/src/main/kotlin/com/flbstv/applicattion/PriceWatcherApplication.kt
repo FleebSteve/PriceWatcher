8fb6894e613302b3e2eafce8a32b83bf3c6204a8
István Székely <szekely.istvan91@gmail.com>
2022-09-24 15:37:46 +0200

    Src moved to backend folder

--- backend/Application/src/main/kotlin/com/flbstv/applicattion/PriceWatcherApplication.kt
+++ backend/Application/src/main/kotlin/com/flbstv/applicattion/PriceWatcherApplication.kt
@@ -3,7 +3,7 @@ package com.flbstv.applicattion
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(scanBasePackages = arrayOf("com.flbstv"))
+@SpringBootApplication(scanBasePackages = ["com.flbstv"])
 class PriceWatcherApplication
 
 fun main(args: Array<String>) {
