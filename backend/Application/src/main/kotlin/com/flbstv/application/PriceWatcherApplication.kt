9051e6abd105d5420925e56f1529afdd00c15bfe
István Székely <szekely.istvan91@gmail.com>
2022-11-04 13:57:45 +0100

    Scheduling enabled

--- backend/Application/src/main/kotlin/com/flbstv/application/PriceWatcherApplication.kt
+++ backend/Application/src/main/kotlin/com/flbstv/application/PriceWatcherApplication.kt
@@ -2,9 +2,11 @@ package com.flbstv.application
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @SpringBootApplication(scanBasePackages = ["com.flbstv"])
+@EnableScheduling
 class PriceWatcherApplication
 
 fun main(args: Array<String>) {
