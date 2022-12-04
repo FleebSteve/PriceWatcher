300de5b100ae2aabae5c33bd2ac85fdc738e3971
István Székely <szekely.istvan91@gmail.com>
2022-12-04 16:18:00 +0100

    ELK indexing

---
+++ backend/Data/src/main/kotlin/com/flbstv/pw/data/DataIndexer.kt
@@ -0,0 +1,49 @@
+package com.flbstv.pw.data
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient
+import co.elastic.clients.json.jackson.JacksonJsonpMapper
+import co.elastic.clients.transport.ElasticsearchTransport
+import co.elastic.clients.transport.rest_client.RestClientTransport
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.flbstv.pw.api.const.KafkaTopics
+import com.flbstv.pw.plugin.api.model.Product
+import org.apache.http.HttpHost
+import org.elasticsearch.client.RestClient
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+
+
+@Service
+class DataIndexer(private val objectMapper: ObjectMapper) {
+
+    private var logger: Logger = LoggerFactory.getLogger(DataIndexer::class.java)
+
+    private val client: ElasticsearchClient
+
+    init {
+
+        val restClient = RestClient.builder(
+            HttpHost("localhost", 9200)
+        ).build()
+
+        val transport: ElasticsearchTransport = RestClientTransport(
+            restClient, JacksonJsonpMapper()
+        )
+
+        client = ElasticsearchClient(transport)
+    }
+
+
+    @KafkaListener(topics = [KafkaTopics.PRODUCT_STREAM], groupId = "product.data.indexer")
+    fun consumeProductStream(message: String) {
+        val product = objectMapper.readValue(message, Product::class.java)
+        client.index { i ->
+            i
+                .index(product.source.lowercase())
+                .id(product.id)
+                .document(product)
+        }
+    }
+}
