a993ae71c10443517c5ce5dbc45f8d61f5fa1585
István Székely <szekely.istvan91@gmail.com>
2023-02-02 21:47:16 +0100

    ProductIdentifier update fix

--- backend/Store/src/main/kotlin/com/flbstv/pw/store/product/identifier/ProductIdentifierServiceImpl.kt
+++ backend/Store/src/main/kotlin/com/flbstv/pw/store/product/identifier/ProductIdentifierServiceImpl.kt
@@ -1,6 +1,5 @@
 package com.flbstv.pw.store.product.identifier
 
-import com.flbstv.pw.api.data.ProductIdentifier
 import com.flbstv.pw.api.service.ProductIdentifierService
 import org.bson.types.ObjectId
 import org.springframework.stereotype.Service
@@ -9,25 +8,25 @@ import org.springframework.stereotype.Service
 class ProductIdentifierServiceImpl(private val productIdentifierRepository: ProductIdentifierRepository) :
     ProductIdentifierService {
 
-    override fun identify(name: String): ProductIdentifier? {
+    override fun identify(name: String): com.flbstv.pw.api.data.ProductIdentifier? {
         return productIdentifierRepository.findByName(name)?.toModel()
     }
 
     override fun add(name: String, source: String, id: String) {
-        val productIdentifier = productIdentifierRepository.findByName(name)
-        if (productIdentifier == null) {
-            productIdentifierRepository.save(
-                ProductIdentifier(
-                    ObjectId(),
-                    name,
-                    mutableMapOf(source to id)
-                )
-            )
-        } else {
-            if(productIdentifier.idMap.containsKey(source)) {
-                productIdentifier.idMap[source] = id
-
-            }
+        val productIdentifier = productIdentifierRepository.findByName(name) ?: saveNewProductIdentifier(name, source, id)
+        if (!productIdentifier.idMap.containsKey(source)) {
+            productIdentifier.idMap[source] = id
+            productIdentifierRepository.save(productIdentifier)
         }
+    }
+
+    private fun saveNewProductIdentifier(name: String, source: String, id: String): ProductIdentifier {
+        return productIdentifierRepository.save(
+            ProductIdentifier(
+                ObjectId(),
+                name,
+                mutableMapOf(source to id)
+            )
+        )
     }
 }
